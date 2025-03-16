84576c2b4e6be1ee2db438122e9d287a6ef056f2
João Brandão <ferrazallex9@gmail.com>
2025-03-16 16:04:36 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,50 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicadores')
+let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
+
+let active = 0
+let firtsPosition = 0
+let lastPosition = items.length - 1
+
+
+function setSlider() {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+    
+
+    let dotsOld = indicator.querySelector('ul li.ativar')
+    dotsOld.classList.remove('ativar')
+    dots[active].classList.add('ativar')
+
+    indicator.querySelector('.numero').innerHTML = '0' + (active + 1)
+
+
+
+}
+
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
+    items[active].classList.add('active')
+
+
+}
+
+
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+
+    active = active - 1 < firtsPosition ? lastPosition : active - 1
+    setSlider()
+    items[active].classList.add('active')
+
+
+}
